5ce8a44e4cccf71487aadd615c6c729f837d5aa0
enumura1 <akirayuto0718@gmail.com>
2024-02-10 13:40:00 +0900

    add:switch page button

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,10 +1,25 @@
+import Button from '@mui/material/Button';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
+import {useNavigate } from 'react-router-dom'
 
 
 export default function Home() {
+
+  
+  const navigate = useNavigate();
+
+
     return (
       <main className="main">
         <div>Home</div>
+        <Button 
+          variant="outlined"
+          size="large"
+          endIcon={<ArrowForwardIcon />}
+          onClick={() => { navigate('login')}}>
+            はじめる
+        </Button>
       </main>
     )
   }
