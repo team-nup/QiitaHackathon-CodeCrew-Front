61a2e266ee1afb716437242e41e7394302e5d0c1
enumura1 <akirayuto0718@gmail.com>
2024-02-10 13:21:33 +0900

    add:riouting routing functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import { Routes, Route } from 'react-router-dom'
 
+import Home from './components/home/Home'
+import SignIn from './components/signin/signin'
+import Login from './components/login/Login'
+import SignUp from './components/signup/siginup'
+import Room from './components/room/room'
+import SelectRoom from './components/selectRoom/selectRoom'
+import Ranking from './components/ranking/ranking'
+import Private from './components/private/private'
+import NotFound from './components/notfound/notfound'
 
-export default function App() {
+import './App.css'
+
+export default function App ()  {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/signin' element={<SignIn/>} />
+      <Route path='/login' element={<Login/>} />
+      <Route path='/signup' element={<SignUp/>} />
+      <Route path='/room' element={<Room/>} />
+      <Route path='/selectRoom' element={<SelectRoom/>} />
+      <Route path='/ranking' element={<Ranking/>} />
+      <Route path='/private' element={<Private/>} />
+      <Route path='*' element={<NotFound/>} />
+    </Routes>
   )
 }
---
+++ src/components/ranking/ranking.tsx
@@ -0,0 +1,8 @@
+
+
+
+export default function Ranking() {
+    return (
+      <div>Ranking</div>
+    )
+}
