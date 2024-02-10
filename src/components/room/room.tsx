a10ea7b939812119a1d5e3d15eff091121275ef8
enumura1 <akirayuto0718@gmail.com>
2024-02-10 19:33:51 +0900

    add:on canvas ai btn

--- src/components/room/room.tsx
+++ src/components/room/room.tsx
@@ -1,4 +1,6 @@
 import Three from '../three/three';
+import IconButton from '@mui/material/IconButton';
+import SmartToyTwoToneIcon from '@mui/icons-material/SmartToyTwoTone';
 
 import RoomPageHeader from '../header/roomPageHeader';
 
@@ -12,6 +14,20 @@ export default function Room() {
         <main className="main">
           <div className='threeContainer'>
             <Three />
+            <div className='AI'>
+                <div className='chatAIBtn'>
+                  <h3>AIに相談する</h3>
+                  <IconButton 
+                    color="primary" 
+                    aria-label="add to shopping cart" 
+                    size="large"
+                    onClick={() => { alert('hoge')}}>
+                      <SmartToyTwoToneIcon 
+                        fontSize="inherit"
+                        style={{ color: 'white' }}/>
+                  </IconButton>
+                </div>
+            </div>
           </div>
         </main>
       </>
